75e1f1239c79cc0d6ddf293bb575ad9c24058b7f
shubham verma <88555802+coderrat1@users.noreply.github.com>
2022-08-13 14:24:52 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Asssignments_C_sharp
+{
+    class SwapNum
+    {
+        public static void Main()
+        {
+            Swapping();
+        }
+        public static void Swapping()
+        {
+            Console.Write("Enter num1: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter num2: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Before swapping num1 = {0} and num2 = {1}", num1, num2);
+            num1 = num1 * num2; //5 * 2 = 10
+            num2 = num1 / num2; //10 / 2 = 5
+            num1 = num1 / num2; //10 / 5 = 2
+            Console.WriteLine();
+            Console.WriteLine("After Swapping num1 = {0} and num2 = {1}", num1, num2);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Asssignments_C_sharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                int result;
+                Console.Write(@"Enter first number: \n");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+                Console.Write("Enter second number: \n");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+                Console.WriteLine("Enter the symbol (+,-,*,/):");
+                string symbol = Console.ReadLine();
+
+
+                switch (symbol)
+                {
+                    case "+":
+                        result = num1 + num2;
+                        Console.WriteLine("Addition : {0}", result);
+                        break;
+                    case "-":
+                        result = num1 - num2;
+                        Console.WriteLine("Substraction : {0}", result);
+                        break;
+                    case "*":
+                        result = num1 * num2;
+                        Console.WriteLine("Multiplication : {0}", result);
+                        break;
+                    case "/":
+                        result = num1 / num2;
+                        Console.WriteLine("Division : {0}", result);
+                        break;
+                    default:
+                        Console.WriteLine("Wrong Input");
+                        break;
+                }
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.GetType());
+            }
+            Console.ReadLine();
+        }
+    }
+}
